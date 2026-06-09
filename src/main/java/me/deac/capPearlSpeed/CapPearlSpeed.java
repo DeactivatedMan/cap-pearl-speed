c3fe21235a210b9821f6167aa79314b7badaaa29
DeactivatedMan <deacmanrbx@gmail.com>
2026-06-09 10:47:44 +0100

    first test

--- src/main/java/me/deac/capPearlSpeed/CapPearlSpeed.java
+++ src/main/java/me/deac/capPearlSpeed/CapPearlSpeed.java
@@ -1,16 +1,43 @@
 package me.deac.capPearlSpeed;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.EnderPearl;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 public final class CapPearlSpeed extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-    }
+    private double maxSpeed;
+    private boolean killPearls;
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public void onEnable() {
+        saveDefaultConfig();
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+
+        maxSpeed = getConfig().getDouble("max-speed");
+        killPearls = getConfig().getBoolean("kill-pearl");
+        long runEvery = getConfig().getLong("run-every");
+
+        Bukkit.getScheduler().runTaskTimer(this, this::IteratePearls, runEvery, runEvery);
+    }
+
+    private void IteratePearls() {
+        for (World world : Bukkit.getWorlds()) {
+            for (EnderPearl pearl : world.getEntitiesByClass(EnderPearl.class)) {
+
+                Vector velocity = pearl.getVelocity();
+
+                if (velocity.length() > maxSpeed) {
+                    getLogger().info("Pearl is speeding! "+ velocity.length());
+
+                    if (killPearls) pearl.remove();
+                    else pearl.setVelocity(velocity.normalize().multiply(maxSpeed));
+                }
+
+            }
+        }
     }
 }
